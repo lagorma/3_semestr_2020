b74259be521a88240267a639aaef4e24e44fdd85
lagorma <anisimova.dm@phystech.edu>
2020-10-06 11:47:25 +0300

    error correction

--- w3/number2.py
+++ w3/number2.py
@@ -1,10 +1,18 @@
 #!/bin/python3
 
+'''
 def write_array(array,file_name):
     with open(array,'r') as input, open(file_name,'w') as output:
         output.write(input.read())
     pass
+'''
+
+def write_array(array,file_name):
+    with open(file_name,'w') as my_file:
+        for line in array:
+            my_file.write(line+'\n')
 
 
+my_array=['abc','cdf','ghi','jkl']
+write_array(my_array,'output.txt')
 
-write_array('input.txt','output.txt')
--- w3/number_4.py
+++ w3/number_4.py
@@ -2,13 +2,16 @@ import json
 
 with open("1.json","r") as myfile:
     data = json.load(myfile)
-    data_initial = data.copy()
     print(json.dumps(data, indent=4, sort_keys=True))
-    to_add = {"week": 3}
     temp = data["glossary"]["GlossDiv"]["GlossList"]["GlossEntry"]
     temp["week"] = 3
+
+with open("1.json","w") as myfile:
+    json.dump(data,myfile,indent=4)
+
 with open("1.json", "w") as myfile:
     myfile.write(json.dumps(data, indent=4))
+
 with open("1.json", "r") as myfile:
     data1 = json.load(myfile)
     print(json.dumps(data1, indent=4, sort_keys=True))
