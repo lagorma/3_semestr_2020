4aa7da994032f93cdca718e7a5385b81955612e1
lagorma <anisimova.dm@phystech.edu>
2020-11-30 23:44:35 +0300

    Ураааа, заработал номер 3

--- w11/w11_3.py
+++ w11/w11_3.py
@@ -1,31 +1,32 @@
 #!/bin/python
-from typing import Any, Union, Coroutine
 import aiohttp
 import asyncio
-from aiofile import LineReader, async_open, AIOFile
+from aiofile import LineReader, AIOFile, Writer
 import re
 
 
-async def help_func(name):
+async def help_func(name,name2):
     async with AIOFile(name, 'r') as file:
         async for line in LineReader(file):
             print(f'I\'m here: {line[:-2:]}')
             async with aiohttp.ClientSession() as session:
                 async with session.get(line) as response:
                     text_file = await response.text()
-                    text_file = re.split(r'[\r\n]', str(text_file))
                     #print(text_file)
-                    print(f'Lines that start with <a >:')
+                    text_file = re.split(r'[\r\n]', str(text_file))
+                    print(f'Lines that start with <a :')
                     for el in text_file:
-                        if el.startswith('<a >'):
+                        if el.strip().startswith('<a '):
                             print(el)
-                            async with AIOFile('str.txt', 'w') as file:
+                            async with AIOFile(name2, 'a') as file:
                                 writer = Writer(file)
-                                await writer(line)
+                                await writer(f'{el}\n')
     print('I finished')
 
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(help_func("urls.txt"))
+    with open('str.txt','w') as f:
+        pass
+    loop.run_until_complete(help_func("urls.txt",'str.txt'))
